d093c1dd6235a704d5398cd57a3756a7dfcf19e2
venh <conf312@naver.com>
2025-05-13 16:31:13 +0900

    Update Solution.java

--- src/main/java/coding/challenge/basic/day13/Solution.java
+++ src/main/java/coding/challenge/basic/day13/Solution.java
@@ -3,37 +3,36 @@ package coding.challenge.basic.day13;
 import java.util.*;
 
 class Solution {
-        public int[] solution(String[] gems) {
-            Set<String> gemTypes = new HashSet<>(Arrays.asList(gems));
-            int totalKinds = gemTypes.size();
+    public int[] solution(String[] gems) {
+        Set<String> gemTypes = new HashSet<>(Arrays.asList(gems));
+        int totalKinds = gemTypes.size();
 
-            Map<String, Integer> gemMap = new HashMap<>();
-            int start = 0, end = 0;
-            int minLen = Integer.MAX_VALUE;
-            int[] answer = {0, gems.length - 1};
+        Map<String, Integer> gemMap = new HashMap<>();
+        int start = 0, end = 0;
+        int minLen = Integer.MAX_VALUE;
+        int[] answer = {0, gems.length - 1};
 
-            while (end < gems.length) {
-                gemMap.put(gems[end], gemMap.getOrDefault(gems[end], 0) + 1);
+        while (end < gems.length) {
+            gemMap.put(gems[end], gemMap.getOrDefault(gems[end], 0) + 1);
 
-                while (gemMap.size() == totalKinds) {
-                    if ((end - start) < minLen) {
-                        minLen = end - start;
-                        answer[0] = start + 1;  // 문제 조건: 1번 진열대부터 시작
-                        answer[1] = end + 1;
-                    }
-
-                    gemMap.put(gems[start], gemMap.get(gems[start]) - 1);
-                    if (gemMap.get(gems[start]) == 0) {
-                        gemMap.remove(gems[start]);
-                    }
-                    start++;
+            while (gemMap.size() == totalKinds) {
+                if ((end - start) < minLen) {
+                    minLen = end - start;
+                    answer[0] = start + 1;  // 문제 조건: 1번 진열대부터 시작
+                    answer[1] = end + 1;
                 }
 
-                end++;
+                gemMap.put(gems[start], gemMap.get(gems[start]) - 1);
+                if (gemMap.get(gems[start]) == 0) {
+                    gemMap.remove(gems[start]);
+                }
+                start++;
             }
 
-            return answer;
+            end++;
         }
+
+        return answer;
     }
 
     public static void main(String[] args) {
